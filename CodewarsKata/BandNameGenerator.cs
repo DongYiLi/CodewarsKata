73387401b7499cfba21357b404312e89c9199bba
Ethan Li <ethan.li@titansoft.com.sg>
2017-08-13 22:18:16 +0800

    BandNameGenerator

---
+++ CodewarsKata/BandNameGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace CodewarsKata
+{
+    public class BandNameGenerator
+    {
+        public string Generator(string input)
+        {
+            input = input.ToLower();
+            var firstLetter = input.Substring(0, 1);
+            var otherLetters = input.Substring(1);
+            if (input.EndsWith(firstLetter))
+            {
+                return string.Concat(firstLetter.ToUpper(), otherLetters, otherLetters);
+            }
+            else
+            {
+                return string.Concat("The ", firstLetter.ToUpper(), otherLetters);
+            }
+        }
+    }
+}
---
+++ CodewarsKataTests/BandNamegeneratorTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using CodewarsKata;
+using NUnit.Framework;
+
+namespace CodewarsKataTests
+{
+    [TestFixture]
+    public class BandNamegeneratorTest
+    {
+        [TestCase("knife", ExpectedResult = "The Knife", TestName = "knife_Should_Be_The Knife")]
+        [TestCase("tart", ExpectedResult = "Tartart", TestName = "tart_Should_Tartart")]
+        [TestCase("sandles", ExpectedResult = "Sandlesandles", TestName = "sandles_Should_Be_Sandlesandles")]
+        [TestCase("bed", ExpectedResult = "The Bed", TestName = "bed_Should_Be_The Bed")]
+        public string SampleTests(string input)
+        {
+            var kata = new BandNameGenerator();
+            return kata.Generator(input);
+        }
+    }
+}
