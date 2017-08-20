4945c7b1b0ac5a7b093f0d76bb8c7a22aa761b8a
Ethan Li <ethan.li@titansoft.com.sg>
2017-08-20 15:34:18 +0800

    Counting Duplicates

---
+++ CodewarsKata/CountingDuplicates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace CodewarsKata
+{
+    public class CountingDuplicates
+    {
+        public int DuplicateCount(string input)
+        {
+            return input.ToLower()
+                        .GroupBy(x => x)
+                        .Count(x => x.Count() > 1);
+        }
+    }
+}
---
+++ CodewarsKataTests/CountingDuplicatesTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using CodewarsKata;
+using NUnit.Framework;
+
+namespace CodewarsKataTests
+{
+    [TestFixture]
+    public class CountingDuplicatesTest
+    {
+        [TestCase("", ExpectedResult = 0, TestName = "Empty_Should_Be_0")]
+        [TestCase("abcde", ExpectedResult = 0, TestName = "abcde_Should_0")]
+        [TestCase("aabbcde", ExpectedResult = 2, TestName = "aabbcde_Should_Be_2")]
+        [TestCase("aabBcde", ExpectedResult = 2, TestName = "aabBcde_Should_Be_2")]
+        [TestCase("Indivisibility", ExpectedResult = 1, TestName = "Indivisibility_Should_Be_1")]
+        [TestCase("Indivisibilities", ExpectedResult = 2, TestName = "Indivisibilities_Should_Be_2")]
+        [TestCase("aA11", ExpectedResult = 2, TestName = "aA11_Should_Be_2")]
+        [TestCase("ABBA", ExpectedResult = 2, TestName = "ABBA_Should_Be_2")]
+        public int SampleTests(string input)
+        {
+            var kata = new CountingDuplicates();
+            return kata.DuplicateCount(input);
+        }
+    }
+}
