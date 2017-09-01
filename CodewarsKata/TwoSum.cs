c5041665ea079e040ccc4489cf4b18cdbe606abe
Ethan Li <ethan.li@titansoft.com.sg>
2017-09-01 23:24:15 +0800

    Add TwoSum

---
+++ CodewarsKata/TwoSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodewarsKata
+{
+    public class TwoSum
+    {
+        public int[] Calculate(int[] nums, int target)
+        {
+            var differences = new int[nums.Length];
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = 0; j < i; j++)
+                {
+                    if (differences[j] == nums[i])
+                    {
+                        return new int[2] { j, i };
+                    }
+                }
+                differences[i] = target - nums[i];
+            }
+            return new int[2] { 0, 0 };
+        }
+    }
+}
---
+++ CodewarsKataTests/TwoSumTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using CodewarsKata;
+using NUnit.Framework;
+
+namespace CodewarsKataTests
+{
+    [TestFixture]
+    public class TwoSumTest
+    {
+        private TwoSum _kata = new TwoSum();
+
+        [TestCase(new[] { 1, 2, 3, 4 }, 9, ExpectedResult = new[] { 0, 0 }, TestName = "1,2,3,4_9_Should_Be_0,0")]
+        [TestCase(new[] { 2, 7, 11, 15 }, 9, ExpectedResult = new[] { 0, 1 }, TestName = "2,7,11,15_9_Should_Be_0,1")]
+        [TestCase(new[] { 3, 2, 4 }, 6, ExpectedResult = new[] { 1, 2 }, TestName = "3,2,4_6_Should_Be_1,2")]
+        public int[] SampleTests(int[] nums, int target)
+        {
+            return _kata.Calculate(nums, target);
+        }
+    }
+}
