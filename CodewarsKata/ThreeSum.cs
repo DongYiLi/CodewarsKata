90aecbc0069b6feac5a0f462402e068398a53f5c
Ethan Li <ethan.li@titansoft.com.sg>
2017-09-09 15:41:28 +0800

    Add ThreeSum

---
+++ CodewarsKata/ThreeSum.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodewarsKata
+{
+    public class ThreeSum
+    {
+        public IList<IList<int>> Calculate(int[] nums)
+        {
+            var result = new List<IList<int>>();
+            Array.Sort(nums);
+            int start;
+            int end;
+            int sum;
+            for (var i = 0; i < nums.Length - 2; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+                sum = 0 - nums[i];
+                start = i + 1;
+                end = nums.Length - 1;
+                while (start < end)
+                {
+                    if (nums[start] + nums[end] == sum)
+                    {
+                        result.Add(new List<int> { nums[i], nums[start], nums[end] });
+                        start = MoveStartElement(nums, start, end);
+                        end = MoveEndElement(nums, start, end);
+                    }
+                    else if (nums[start] + nums[end] < sum)
+                    {
+                        start = MoveStartElement(nums, start, end);
+                    }
+                    else
+                    {
+                        end = MoveEndElement(nums, start, end);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private int MoveEndElement(int[] nums, int start, int end)
+        {
+            while (nums[end] == nums[--end] && start < end) { }
+
+            return end;
+        }
+
+        private int MoveStartElement(int[] nums, int start, int end)
+        {
+            while (nums[start] == nums[++start] && start < end) { }
+
+            return start;
+        }
+    }
+}
---
+++ CodewarsKataTests/ThreeSumTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using CodewarsKata;
+using NUnit.Framework;
+
+namespace CodewarsKataTests
+{
+    [TestFixture]
+    public class ThreeSumTest
+    {
+        private ThreeSum _kata = new ThreeSum();
+
+        [TestCaseSource(typeof(Test), "TestCases")]
+        public IList<IList<int>> SampleTests(int[] nums)
+        {
+            return _kata.Calculate(nums);
+        }
+
+        public class Test
+        {
+            public static IEnumerable TestCases
+            {
+                get
+                {
+                    yield return new TestCaseData(new[] { 0, 0, 0, 0 }).Returns(new List<List<int>> { new List<int> { 0, 0, 0 } }).SetName("0, 0, 0, 0_Should_Be_{ {0, 0, 0} }");
+                    yield return new TestCaseData(new[] { -2, 0, 0, 2, 2 }).Returns(new List<List<int>> { new List<int> { -2, 0, 2 } }).SetName("-2, 0, 0, 2, 2_Should_Be_{ {-2, 0, 2} }");
+                    yield return new TestCaseData(new[] { -1, 0, 1, 2, -1, -4 }).Returns(new List<List<int>> { new List<int> { -1, -1, 2 }, new List<int> { -1, 0, 1 } }).SetName("-1, 0, 1, 2, -1, -4_Should_Be_{ { -1, -1, 2 }, { -1, 0, 1 } }");
+                }
+            }
+        }
+    }
+}
