b1606763d909ed1238d64dd781b2b32042a6d4e8
Ethan Li <ethan.li@titansoft.com.sg>
2017-11-03 17:42:37 +0800

    Add BowlingGame

---
+++ CodewarsKata/BowlingGame.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodewarsKata
+{
+    public class BowlingGame
+    {
+        public int Calculate(int[] nums)
+        {
+            var rounds = new List<Round>();
+            for (int i = 0; i < nums.Length && i < 23; i = i + 2)
+            {
+                rounds.Add(new Round() { First = nums[i], Second = nums[i + 1] });
+            }
+            return rounds.Select((x, i) =>
+            {
+                if (x.Status == Status.Spare)
+                {
+                    return x.NormalScore + rounds[i + 1].First;
+                }
+                return x.NormalScore;
+            }).Sum();
+        }
+
+        public enum Status
+        {
+            Normal,
+            Spare,
+            Strike
+        }
+
+        public class Round
+        {
+            public int First { get; set; }
+
+            public int NormalScore => First + Second;
+
+            public int Second { get; set; }
+
+            public Status Status
+            {
+                get
+                {
+                    if (First == 10)
+                    {
+                        return Status.Strike;
+                    }
+                    return First + Second == 10 ? Status.Spare : Status.Normal;
+                }
+            }
+
+            public int Third { get; set; }
+        }
+    }
+}
---
+++ CodewarsKataTests/BowlingGameTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using CodewarsKata;
+using NUnit.Framework;
+
+namespace CodewarsKataTests
+{
+    [TestFixture]
+    public class BowlingGameTest
+    {
+        private readonly BowlingGame _kata = new BowlingGame();
+
+        [TestCase(new[] { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, ExpectedResult = 2, TestName = "1,1_Should_Be_2")]
+        [TestCase(new[] { 1, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, ExpectedResult = 10, TestName = "1,9_Should_Be_10")]
+        [TestCase(new[] { 1, 9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, ExpectedResult = 12, TestName = "1,9,1_Should_Be_12")]
+        [TestCase(new[] { 1, 9, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, ExpectedResult = 14, TestName = "1,9,1,2_Should_Be_14")]
+        [TestCase(new[] { 1, 9, 1, 9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, ExpectedResult = 23, TestName = "1,9,1,9,1_Should_Be_23")]
+        public int SampleTests(int[] nums)
+        {
+            return _kata.Calculate(nums);
+        }
+    }
+}
