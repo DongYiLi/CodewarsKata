870f6f435df6cc57fdbc97499b2580555e6e0a86
Ethan Li <ethan.li@titansoft.com.sg>
2017-11-03 16:43:00 +0800

    Add ContainsDuplicateII

---
+++ CodewarsKata/ContainsDuplicateII.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CodewarsKata
+{
+    public class ContainsDuplicateII
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            var result = nums.Select((x, index) => new { Index = index, Value = x }).GroupBy(x => x.Value)
+                .Where(x => x.Count() > 1)
+                .Select(x => x.Aggregate(new Index
+                {
+                    Now = x.First().Index - x.Max(y => y.Index),
+                    Min = k + 1
+                }, (accumulate, y) =>
+                {
+                    accumulate.Min = Math.Min(y.Index - accumulate.Now, accumulate.Min);
+                    accumulate.Now = y.Index;
+                    return accumulate;
+                }, acct => acct.Min)).Any(x => x <= k);
+            return result;
+        }
+
+        public class Index
+        {
+            public int Min { get; set; }
+            public int Now { get; set; }
+        }
+    }
+}
---
+++ CodewarsKataTests/ContainsDuplicateIITest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using CodewarsKata;
+using NUnit.Framework;
+
+namespace CodewarsKataTests
+{
+    [TestFixture]
+    public class ContainsDuplicateIITest
+    {
+        [TestCase(new int[] { 1, 2, 3, 1 }, 3, ExpectedResult = true, TestName = "1,2,3,1_3_Should_Be_True")]
+        [TestCase(new int[] { 99, 99 }, 2, ExpectedResult = true, TestName = "9,9_2_Should_Be_True")]
+        [TestCase(new int[] { 1 }, 1, ExpectedResult = false, TestName = "1_1_Should_Be_False")]
+        [TestCase(new int[] { 1, 0, 1, 1 }, 1, ExpectedResult = true, TestName = "1,0,1,1_1_Should_Be_True")]
+        [TestCase(new int[] { 1, 2, 1 }, 0, ExpectedResult = false, TestName = "1,2,1_0_Should_Be_False")]
+        [TestCase(new int[] { 1, 2, 1 }, 1, ExpectedResult = false, TestName = "1,2,1_1_Should_Be_False")]
+
+        public bool SampleTests(int[] input, int k)
+        {
+            var kata = new ContainsDuplicateII();
+            return kata.ContainsNearbyDuplicate(input, k);
+        }
+    }
+}
